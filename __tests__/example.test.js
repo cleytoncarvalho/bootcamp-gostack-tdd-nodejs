33fecd7aaa5d94d04657ee834d076114c27228fd
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2019-10-08 20:34:49 -0300

    Implementing faker and factory-girl

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -1,9 +1,9 @@
-function soma(a, b) {
+function sum(a, b) {
   return a + b;
 }
 
-test('if I call soma() whith 4 and 5 it should return 9', () => {
-  const result = soma(4, 5);
+test('if I call sum() whith 4 and 5 it should return 9', () => {
+  const result = sum(4, 5);
 
   expect(result).toBe(9);
 });
